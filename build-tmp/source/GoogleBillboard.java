b61824a1c04683b24a53c7553c6020ab54cc2a06
unknown <thwhite@697-334-Boromir.sf.k12.internal>
2014-12-10 10:10:39 -0800

    First commit

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,17 +15,41 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
-public void setup()  
+public void setup()  //2.718281828
 {            
-     noLoop();  
+     noLoop(); 
+     int i = 3;
+     boolean noPrime = true;
+
+     while (noPrime) {
+     	String digits = e.substring(i, i + 10);
+     	Double dNum = Double.parseDouble(digits);
+     	
+     	if (isPrime(dNum)) {
+     		//noPrime = false;
+     		System.out.println(dNum);
+     		break;
+     	}
+
+     	if (i == 100) {
+     		break;
+     	}
+
+     	i ++;
+     }
 }  
 public void draw()  
 {   
 	//not needed for this assignment
 }  
 public boolean isPrime(double dNum)  
-{   
-    //to be finished later   
+{    
+    for (int i = 2; i <= Math.sqrt(dNum); i ++) {
+    	//System.out.println(Math.sqrt(dNum));
+    	if ( dNum % i == 0 ) {
+    		return false;
+    	}
+    }
     return true;  
 } 
   static public void main(String[] passedArgs) {
